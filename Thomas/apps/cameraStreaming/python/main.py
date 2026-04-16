c3529bca96447d962949a1e85812ec99190e8222
thomaslenges <thomaslenges@hotmail.com>
2026-04-16 06:44:28 +0000

    Add readme

--- Thomas/apps/cameraStreaming/python/main.py
+++ Thomas/apps/cameraStreaming/python/main.py
@@ -1,6 +1,7 @@
 import os
 from arduino.app_utils import App
 from bricks.camera.camera import Camera
+
 #from bricks.web_ui.web_ui import WebUI
 from arduino.app_bricks.web_ui import WebUI
 import cv2
@@ -14,24 +15,12 @@ ui = WebUI()
 
 def streaming():
     image = camera.capture()
-    #print("streaming!")
 
     if image is None:
-        #print("No frame received")
         return
 
-    ok = cv2.imwrite("assets/latest.jpg", image)
-    #print(f"Saved???: {ok}")
+    cv2.imwrite("assets/latest.jpg", image)
 
 
 # Run the application
 App.run(streaming)
-
-
-# from arduino.app_utils import App
-# from bricks.webServer.web_ui import WebUI
-
-# # Initialize the Web UI server
-# ui = WebUI()
-
-# App.run()
